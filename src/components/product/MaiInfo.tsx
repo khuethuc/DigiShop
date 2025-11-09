d502e386a4c087a61407cce1ee01732dea4bc0c3
Ha An <daothihaan@gmail.com>
2025-11-09 15:43:59 +0700

    little fix

--- src/components/product/MaiInfo.tsx
+++ src/components/product/MaiInfo.tsx
@@ -38,7 +38,55 @@ export default function MainInfo(props: MainInfoProps) {
 
   return (
     <Stack spacing={2.5}>
-      {/* ... info ... */}
+      {/* Main Info Section */} 
+      <Box>
+        <Typography
+          variant="h1"
+          sx={{ fontSize: { xs: 25, md: 35, lg: 40 }, mb: 5 }}
+        >
+          {title}
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          Status: {quantity > 0 ? "In Stock" : "Out of stock"}
+        </Typography>
+
+        <Stack direction="row" sx={{ marginBottom: 2 }}>
+          <Typography variant="body1" gutterBottom>
+            Category:
+          </Typography>
+          <KeywordTag label={category} size="md" mgLeft={3} />
+        </Stack>
+
+        <Stack direction="row" spacing={5} mb={2}>
+          {/* Discount Price */}
+          {discount_price || max_discount_price ? (
+            <Typography
+              fontWeight={700}
+              sx={{ fontSize: { xs: 13, sm: 15, md: 17, lg: 18 }, color: "red" }}
+            >
+              {discount_price ? discount_price.toLocaleString("en-US") : "-"} ₫
+              {max_discount_price
+                ? ` - ${max_discount_price.toLocaleString("en-US")} ₫`
+                : ""}
+            </Typography>
+          ) : null}
+
+          {/* Original Price */}
+          {original_price ? (
+            <Typography
+              color="textDisabled"
+              sx={{ textDecoration: "line-through" }}
+            >
+              {original_price.toLocaleString("en-US")} ₫
+              {validMaxOriginalPrice !== null
+                ? ` - ${validMaxOriginalPrice.toLocaleString("en-US")} ₫`
+                : ""}
+            </Typography>
+          ) : null}
+        </Stack>
+      </Box>      
+
       <Divider sx={{ borderColor: "#68686880", borderBottomWidth: 1 }} />
 
       {/* Packages */}
